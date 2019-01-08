599c37ac5feb0b074ec35d317f348895ff9aa193
RafPe <rafal@pieniazek.nl>
2019-01-08 09:53:29 +0100

    Refactor reporting

--- reporting.go
+++ reporting.go
@@ -3,35 +3,52 @@ package edgegrid
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type ReportingService struct {
 	client *Client
 }
 
-type AkamaiReportingBody struct {
+type ReportingBody struct {
 	ObjectType string   `json:"objectType"`
 	ObjectIds  []string `json:"objectIds"`
 	Metrics    []string `json:"metrics"`
 }
 
-type AkamaiReportingBodyAll struct {
+type ReportingBodyAll struct {
 	ObjectType string   `json:"objectType"`
 	ObjectIds  string   `json:"objectIds"`
 	Metrics    []string `json:"metrics"`
 }
 
-type AkamaiReportOptions struct {
+//TODO: Change TypeOfReport into string consts ?
+//ReportOptions represents options available for report generation
+type ReportOptions struct {
 	TypeOfReport string
 	Interval     string
-	DateRange    string
+	Start        time.Time
+	End          time.Time
 }
 
-func (nls *ReportingService) GenerateReport(body interface{}, opts AkamaiReportOptions) (*ClientResponse, error) {
+// QStrReporting includes query params used for reporting
+type QStrReporting struct {
+	Start    time.Time `url:"start,omitempty"`
+	End      time.Time `url:"end,omitempty"`
+	Interval string    `url:"interval,omitempty"`
+}
 
-	apiURI := fmt.Sprintf("%s/%s/versions/1/report-data?%s&interval=%s", ReportingPathV1, opts.TypeOfReport, opts.DateRange, opts.Interval)
+//GenerateReport Calls reporing API to generate given report based on provided request
+func (nls *ReportingService) GenerateReport(body interface{}, opts ReportOptions) (*ClientResponse, error) {
+	// since this call is tricky with query params we are
+	qParams := QStrReporting{
+		Interval: opts.Interval,
+		End:      opts.End,
+		Start:    opts.Start,
+	}
 
-	resp, err := nls.client.NewRequest(http.MethodPost, apiURI, body, nil)
+	path := fmt.Sprintf("%s/%s/versions/1/report-data", ReportingPathV1, opts.TypeOfReport)
+	resp, err := nls.client.makeAPIRequest(http.MethodPost, path, qParams, nil, body, nil)
 
 	return resp, err
 
